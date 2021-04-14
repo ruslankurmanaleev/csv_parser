b4e27d10c03d45778788a16d803ee9d5231d3a40
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2021-04-14 20:10:21 +0300

    Add Service Object PriceList

---
+++ app/services/price_list.rb
@@ -0,0 +1,98 @@
+require "csv"
+
+class PriceList
+  # Change encoding and delimiter if required
+  ENCODING = "windows-1251".freeze
+  DELIMITER = ";".freeze
+
+  def initialize(name, file)
+    @name = name
+    @file_path = file
+    @skipped_counter = 0
+    @created_counter = 0
+  end
+
+  def parse
+    # We need to clean all existing data with the same PriceList name
+    erase_price_list_if_exists!
+
+    # With each line of CSV file parse product with headers, so we can have a Hash later
+    CSV.foreach(@file_path, col_sep: DELIMITER, row_sep: :auto, headers: true, nil_value: nil, quote_char: "|", encoding: ENCODING) do |product|
+      # It's easier and more explicit to work with Hash here instead of Array
+      product = product.to_hash
+
+      # Skip lines with blank params
+      next if blank_params?(product)
+
+      # Update stock for the product if it's required
+      product["Кол-во"] = format_stock(product["Кол-во"]) if need_to_format_stock?(product["Кол-во"])
+
+      # Find existing Product with UID
+      existing_product = fetch_existing_product_or_nil(product)
+
+      next update_product!(existing_product, product) unless existing_product.nil?
+
+      create_product!(product)
+    end
+
+    "#{@skipped_counter}, #{@created_counter}"
+  end
+
+  private
+
+  # Destroys object by given pattern one by one
+  def erase_price_list_if_exists!
+    Product.where(price_list: @name).each { |list| list.destroy }
+  end
+
+  # If any of these params are nil, then skip the product
+  def blank_params?(obj)
+    obj["Производитель"].strip.blank? || obj["Номер"].blank? || obj["Наименование"].blank? || obj["Цена"].blank? || obj["Кол-во"].blank?
+  end
+
+  # Returns nil or Product that exists, and that has to be updated
+  def fetch_existing_product_or_nil(product)
+    # We might have a few UIDs but with different PriceLists
+    products = Product.where(uid: build_uid(product))
+
+    return nil if products.nil?
+
+    # If we've got the unique, return it, otherwise return nil
+    products.where(price_list: @name).any? ? products.first : nil
+  end
+
+  # Updates product. Because of we have a few params, it's faster to update them all instead of parsing and updating only different
+  def update_product!(existing_product, new_product_hash)
+    existing_product.update(brand: new_product_hash["Производитель"].strip, code: new_product_hash["Номер"].strip,
+                            stock: new_product_hash["Кол-во"].strip,
+                            name: new_product_hash["Наименование"].strip, cost: new_product_hash["Цена"].strip)
+
+    # Add +1 to skipped counter for stats
+    @skipped_counter += 1
+  end
+
+  # Creates the product
+  def create_product!(product_hash)
+    Product.new(brand: product_hash["Производитель"].strip, code: product_hash["Номер"].strip, stock: product_hash["Кол-во"].strip,
+                name: product_hash["Наименование"].strip, cost: product_hash["Цена"].strip,
+                uid: build_uid(product_hash), price_list: @name).save
+
+    # Update this counter as well
+    @created_counter += 1
+  end
+
+  # UID must be unique for one PriceList, and it must be "register-indepent", so downcase it as parsing via DB is slower
+  def build_uid(product)
+    "#{product['Производитель']}#{product['Номер']}".downcase
+  end
+
+  # If the stock has something like "> 10" (more than 10)
+  def need_to_format_stock?(stock)
+    stock.split.include?(">")
+  end
+
+  # If the stock has something like "> 10" (more than 10), leave just the number
+  def format_stock(stock)
+    stock.split.gsub(">", "").join.strip
+  end
+end
