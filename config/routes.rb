738ea4738be83924884926484bf206856f0e740b
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2021-04-14 20:08:38 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "price_lists#new"
+
+  resources :price_lists, only: %i[new create]
 end
