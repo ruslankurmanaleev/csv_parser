ede1c3e858cb14400b563aa5351d21edea402dfe
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2021-04-14 16:56:48 +0300

    Add migration (no indexes yet)

---
+++ db/migrate/20210414133853_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :price_list
+      t.string :brand, null: false
+      t.string :code, null: false
+      t.integer :stock, default: 0, null: false
+      t.decimal :cost, precision: 12, scale: 2, null: false
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
